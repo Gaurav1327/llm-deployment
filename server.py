511481ecc718dd65dc4e67cbb35e24b199f25787
yashica <yashica.dhalla121@gmail.com>
2024-04-29 23:47:01 +0530

    fix version

--- server.py
+++ server.py
@@ -5,10 +5,10 @@ import subprocess
 NGROK_AUTH_TOKEN = os.environ.get('NGROK_AUTH_TOKEN', '23baKlcVFtF3OkJtNrD6xExrYlI_7ZVM8X8z3bG6s3Zvt3Jgc')
 
 # Start the FastChat services
-subprocess.Popen(['python', '-m', 'fastchat.serve.controller', '--host', '0.0.0.0'])
-subprocess.Popen(['python', '-m', 'fastchat.serve.model_worker', '--model-path', '../chatglm2-6b', '--host', '0.0.0.0'])
-subprocess.Popen(['python', '-m', 'fastchat.serve.openai_api_server', '--host', '0.0.0.0'])
-subprocess.Popen(['python', '-m', 'fastchat.serve.gradio_web_server', '--host', '0.0.0.0'])
+subprocess.Popen(['python3', '-m', 'fastchat.serve.controller', '--host', '0.0.0.0'])
+subprocess.Popen(['python3', '-m', 'fastchat.serve.model_worker', '--model-path', '../chatglm2-6b', '--host', '0.0.0.0'])
+subprocess.Popen(['python3', '-m', 'fastchat.serve.openai_api_server', '--host', '0.0.0.0'])
+subprocess.Popen(['python3', '-m', 'fastchat.serve.gradio_web_server', '--host', '0.0.0.0'])
 
 # Start ngrok
 ngrok_process = subprocess.Popen(['ngrok', 'http', '8002', '--auth', NGROK_AUTH_TOKEN], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
